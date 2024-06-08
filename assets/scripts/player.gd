051ab6df46c67b437d2aff1d720de9803754ce9e
Crambeary <3926951+Crambeary@users.noreply.github.com>
2024-06-08 14:39:21 -0500

    player movement set for inputs and jetpack feeling
    also added .gitignore to ignore .godot folder
    Animation placeholder added for flight, running only on the floor
    Jetpack uses acceleration instead of velocity

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
+@onready var animated_sprite = $AnimatedSprite2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JET_ACCELERATION = -40.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,10 +13,13 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	else:
+		animated_sprite.play("run")
+		
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if (Input.is_action_pressed("jet") or Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)) and not is_on_ceiling():
+		velocity.y += JET_ACCELERATION
+		animated_sprite.play("fly")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.

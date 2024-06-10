d1dfbcbd8f1fd89ebe8e1415fc98a9d0c3142bba
Crambeary <3926951+Crambeary@users.noreply.github.com>
2024-06-10 12:07:48 -0500

    snappier movemen

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JET_ACCELERATION = -40.0
+const JET_ACCELERATION = -50.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -13,6 +13,7 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		velocity.y = clamp(velocity.y, -500, 500)
 	else:
 		animated_sprite.play("run")
 		

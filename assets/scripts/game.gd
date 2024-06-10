143d2570bbbcfe6d6d14e05437277cdaf9bc8ffb
Crambeary <3926951+Crambeary@users.noreply.github.com>
2024-06-10 14:32:40 -0500

    obstacle kills player

--- assets/scripts/game.gd
+++ assets/scripts/game.gd
@@ -14,5 +14,5 @@ func _process(delta):
 	pass
 
 func _hit():
-	print("Hit")
+	player.death()
 	pass
--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 @onready var animated_sprite = $AnimatedSprite2D
 
 
+
 const SPEED = 300.0
 const JET_ACCELERATION = -50.0
 
@@ -9,6 +10,8 @@ signal hit
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var direction = 1 # Run to the right
+var is_dead := false
 
 
 func _physics_process(delta):
@@ -16,7 +19,7 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		velocity.y = clamp(velocity.y, -500, 500)
-	else:
+	elif is_on_floor() and direction == 1:
 		animated_sprite.play("run")
 		
 	# Handle jump.
@@ -26,7 +29,11 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = 1
+
+	if direction < 0 and is_dead:
+		direction += 0.8 * delta
+	elif direction > 0 and is_dead:
+		direction = 0
 	if direction:
 		velocity.x = direction * SPEED
 	else:
@@ -34,4 +41,8 @@ func _physics_process(delta):
 
 	move_and_slide()
 
-	
+func death():
+	animated_sprite.play("death")
+	direction = -1
+	is_dead = true
+
